a7d59becfbb571442d9feb595e78e7e22decd1b1
LuisDavidSanchezOvalles <59859052+LuisDavidSanchezOvalles@users.noreply.github.com>
2020-10-06 16:45:47 -0400

    Agregando Clientes

---
+++ SweetDreams/SweetDreams.Admin/Models/Clientes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetDreams.Admin.Models
+{
+    public class Clientes
+    {
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public bool Accesibilidad { get; set; }
+        public Clientes()
+        {
+            ClienteId = 0;
+            Nombres = string.Empty;
+            NombreUsuario = string.Empty;
+            Telefono = string.Empty;
+            Correo = string.Empty;
+            Clave = string.Empty;
+            Accesibilidad = true;
+        }
+
+        public Clientes(int clienteId, string nombres, string nombreUsuario, string telefono, string correo, string clave, bool accesibilidad)
+        {
+            ClienteId = clienteId;
+            Nombres = nombres;
+            NombreUsuario = nombreUsuario;
+            Telefono = telefono;
+            Correo = correo;
+            Clave = clave;
+            Accesibilidad = accesibilidad;
+        }
+    }
+}
---
+++ SweetDreams/SweetDreams.Api/Models/Administrador/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweetDreams.Api.Models.Administrador
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombres { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+        public bool Accesibilidad { get; set; }
+    }
+}
