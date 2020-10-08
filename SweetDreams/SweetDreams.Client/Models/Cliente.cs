21fdc7313d4f85b9f55eaca027f1e67db23f8606
Emminton Manuel Ure�a Santana <emminton_urena@ucne.edu.do>
2020-10-07 21:35:32 -0400

    Agregando Atributos

--- SweetDreams/SweetDreams.Client/Models/Cliente.cs
+++ SweetDreams/SweetDreams.Client/Models/Cliente.cs
@@ -7,5 +7,33 @@ namespace SweetDreams.Client.Models
 {
     public class Cliente
     {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string NumeroTelefono { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Correo { get; set; }
+        public string Clave { get; set; }
+
+        public Cliente()
+        {
+            ClienteId = 0;
+            Nombre = string.Empty;
+            NumeroTelefono = string.Empty;
+            NombreUsuario = string.Empty;
+            Correo = string.Empty;
+            Clave = string.Empty;
+        }
+
+        public Cliente(int clienteId, string nombre, string numeroTelefono, string nombreUsuario, string correo, string clave)
+        {
+            ClienteId = clienteId;
+            Nombre = nombre ?? throw new ArgumentNullException(nameof(nombre));
+            NumeroTelefono = numeroTelefono ?? throw new ArgumentNullException(nameof(numeroTelefono));
+            NombreUsuario = nombreUsuario ?? throw new ArgumentNullException(nameof(nombreUsuario));
+            Correo = correo ?? throw new ArgumentNullException(nameof(correo));
+            Clave = clave ?? throw new ArgumentNullException(nameof(clave));
+        }
+
     }
+    
 }
