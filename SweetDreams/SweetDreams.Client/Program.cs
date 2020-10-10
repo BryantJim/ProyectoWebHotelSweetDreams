2ab15bc083aa60dba3184a6c87c599c60b4db9b0
Anthony Brian Jiménez López <60039575+BryantJim@users.noreply.github.com>
2020-10-10 15:45:03 -0400

    Cambios en clientes que funciona

--- SweetDreams/SweetDreams.Client/Program.cs
+++ SweetDreams/SweetDreams.Client/Program.cs
@@ -14,13 +14,15 @@ namespace SweetDreams.Client
     public class Program
     {
         public static async Task Main(string[] args)
-        {
-            
+        {           
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazoredToast();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
             await builder.Build().RunAsync();
         }
     }
