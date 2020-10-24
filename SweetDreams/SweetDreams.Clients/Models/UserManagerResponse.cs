9927fb9ef8fa32b1f2ed7521e1451bbc0b8b41a0
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-10-24 12:02:24 -0400

    arreglos

---
+++ SweetDreams/SweetDreams.Clients/Models/UserManagerResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace SweetDreams.Clients.Models
+{
+    public class UserManagerResponse
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccess { get; set; }
+
+        public string[] Errors { get; set; }
+
+        public Dictionary<string, string> UserInfo { get; set; }
+
+        public DateTime? ExpireDate { get; set; }
+    }
+}
