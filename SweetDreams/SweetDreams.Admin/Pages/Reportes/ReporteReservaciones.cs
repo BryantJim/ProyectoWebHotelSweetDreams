da5159e3d79d7a72007670c9fbe97fe1b3caa96c
Ga1ileo <59858318+Ga1ileo@users.noreply.github.com>
2020-10-31 14:36:16 -0400

    * ReporteReservaciones.cs:
    * C_Clientes.razor: Se agrego el reporte Reservaciones

---
+++ SweetDreams/SweetDreams.Admin/Pages/Reportes/ReporteReservaciones.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using SweetDreams.Admin.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+using SweetDreams.Admin;
+
+namespace SweetDreams.Admin.Pages.Reportes
+{
+    public class ReporteReservaciones
+    {
+        int columnas = 11;
+
+        HttpClient Http;
+
+        Document document = new Document();
+        PdfPTable pdfTable;
+        PdfPCell pdfCell = new PdfPCell();
+        Font fontStyle, fontFecha, fontTitulo;
+
+        MemoryStream memoryStream = new MemoryStream();
+
+        List<Reservaciones> listaReservaciones = new List<Reservaciones>();
+
+
+        public byte[] Reporte(List<Reservaciones> lista)
+        {
+
+            listaReservaciones = lista;
+
+            document = new Document(PageSize.Letter, 25f, 25f, 20f, 20f);
+            pdfTable = new PdfPTable(columnas);
+
+            pdfTable.WidthPercentage = 100;
+            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+            fontStyle = FontFactory.GetFont("Calibri", 8f, 1);
+
+            PdfWriter.GetInstance(document, memoryStream);
+            document.Open();
+
+            float[] anchoColumnas = new float[columnas];
+
+            anchoColumnas[0] = 100; //ReservacionesId
+            anchoColumnas[1] = 100; //Nombre de Cliente
+            anchoColumnas[2] = 100; // Fecha Inicio
+            anchoColumnas[3] = 100; //Fecha Salida
+            anchoColumnas[4] = 100; //Tarjeta
+            anchoColumnas[5] = 100; //Balance
+
+            pdfTable.SetWidths(anchoColumnas);
+
+            this.ReportHeader();
+            this.ReportBody();
+
+            pdfTable.HeaderRows = 1;
+            document.Add(pdfTable);
+            document.Close();
+
+            return memoryStream.ToArray();
+        }
+
+        private void ReportHeader()
+        {
+
+            // AÑADIR EL LOGO DE LA EMPRESA
+
+            // pdfCell = new PdfPCell(this.AddLogo());
+            // pdfCell.Colspan = 1;
+            // pdfCell.Border = 0;
+            // pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(this.setPageTitle());
+            pdfCell.Colspan = columnas;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+        }
+
+        // private PdfPTable AddLogo()
+        // {
+        //     int maxColumn = 1;
+        //     PdfPTable pdfPTable = new PdfPTable(maxColumn);
+        //     string img = $"{Directory.GetCurrentDirectory()}{@"/wwwroot/Images/logo.png"}";
+        //     Image image = Image.GetInstance(img);
+
+        //     pdfCell = new PdfPCell(image);
+        //     pdfCell.Colspan = maxColumn;
+        //     pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+        //     pdfCell.Border = 0;
+        //     pdfCell.ExtraParagraphSpace = 0;
+        //     pdfPTable.AddCell(pdfCell);
+
+        //     pdfPTable.CompleteRow();
+
+        //     return pdfPTable;
+        // }
+
+        private PdfPTable setPageTitle()
+        {
+            PdfPTable pdfTable = new PdfPTable(2);
+
+            fontStyle = FontFactory.GetFont("Calibri", 18f, 1);
+            fontFecha = FontFactory.GetFont("Calibri", 10f, 1);
+            fontTitulo = FontFactory.GetFont("Calibri", 25f, 1);
+
+            pdfCell = new PdfPCell(new Phrase("SweetDereams, srl", fontTitulo));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Colspan = 2;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            pdfCell = new PdfPCell(new Phrase("Reporte de Reservaciones", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Colspan = 2;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            pdfCell = new PdfPCell(new Phrase(DateTime.Now.ToString("dd/MM/yyyy H:mm tt"), fontFecha));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Colspan = 2;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            //Una fila en blanco
+            pdfCell = new PdfPCell(new Phrase(" ", fontStyle));
+            pdfCell.Colspan = 2;
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            return pdfTable;
+        }
+
+        private void ReportBody()
+        {
+            fontStyle = FontFactory.GetFont("Calibri", 9f, 1);
+            var _fontStyle = FontFactory.GetFont("Calibri", 9f, 0);
+
+            #region Table Header
+            pdfCell = new PdfPCell(new Phrase("ReservacionesId", fontStyle)); //ReservacionesId
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Nombre de Cliente", fontStyle)); //Nombre de cliente
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Fecha Inicio", fontStyle)); //fecha Inicio
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Fecha Salida", fontStyle)); //Fecha Salida
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Tarjeta", fontStyle)); //Tarjeta
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Balance", fontStyle)); //Balance
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+
+
+
+            pdfTable.CompleteRow();
+            #endregion
+
+            #region Table Body
+            int num = 0;
+
+            foreach (var item in listaReservaciones)
+            {
+                num++;
+                pdfCell = new PdfPCell(new Phrase(item.ReservacionId.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.NombreCliente.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.FechaInicio.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.FechaSalida.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Tarjeta.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Balance.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+
+
+                pdfTable.CompleteRow();
+            }
+
+            pdfCell = new PdfPCell(new Phrase("Total de Reservaciones", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(num++.ToString(), fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("            ", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+            #endregion
+        }
+    }
+}
