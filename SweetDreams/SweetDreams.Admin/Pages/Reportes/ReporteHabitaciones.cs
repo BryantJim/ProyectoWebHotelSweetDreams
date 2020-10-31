3ec6d708dcf86111622a5e0d2399e622ec5f88a1
Ga1ileo <59858318+Ga1ileo@users.noreply.github.com>
2020-10-31 14:09:11 -0400

    * ReporteHabitaciones.cs:
    * C_Clientes.razor: Se agrego El reporte de Habitacion

---
+++ SweetDreams/SweetDreams.Admin/Pages/Reportes/ReporteHabitaciones.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using SweetDreams.Admin.Reportes;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using SweetDreams.Admin.Models;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+
+namespace SweetDreams.Admin.Pages.Reportes
+{
+    public class ReporteHabitaciones
+    {
+
+        int columnas = 11;
+
+        HttpClient Http;
+
+        Document document = new Document();
+        PdfPTable pdfTable;
+        PdfPCell pdfCell = new PdfPCell();
+        Font fontStyle, fontFecha, fontTitulo;
+
+        MemoryStream memoryStream = new MemoryStream();
+
+        List<Habitacion> ListaHabitaciones = new List<Habitacion>();
+
+
+        public byte[] Reporte(List<Habitacion> lista)
+        {
+
+            ListaHabitaciones = lista;
+
+            document = new Document(PageSize.Letter, 25f, 25f, 20f, 20f);
+            pdfTable = new PdfPTable(columnas);
+
+            pdfTable.WidthPercentage = 100;
+            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+            fontStyle = FontFactory.GetFont("Calibri", 8f, 1);
+
+            PdfWriter.GetInstance(document, memoryStream);
+            document.Open();
+
+            float[] anchoColumnas = new float[columnas];
+
+            anchoColumnas[0] = 100; //Esta sera la fila 1 ClienteId
+            anchoColumnas[1] = 100; //Esta sera la fila 2 Nombre
+            anchoColumnas[2] = 100; //Telefono
+            anchoColumnas[3] = 100; // Nombre de usuario
+            anchoColumnas[4] = 100; //Correo
+            anchoColumnas[5] = 100;
+            anchoColumnas[6] = 100;
+            anchoColumnas[7] = 100;
+
+            pdfTable.SetWidths(anchoColumnas);
+
+            this.ReportHeader();
+            this.ReportBody();
+
+            pdfTable.HeaderRows = 1;
+            document.Add(pdfTable);
+            document.Close();
+
+            return memoryStream.ToArray();
+        }
+
+        private void ReportHeader()
+        {
+
+            // AÑADIR EL LOGO DE LA EMPRESA
+
+            // pdfCell = new PdfPCell(this.AddLogo());
+            // pdfCell.Colspan = 1;
+            // pdfCell.Border = 0;
+            // pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(this.setPageTitle());
+            pdfCell.Colspan = columnas;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+        }
+
+        // private PdfPTable AddLogo()
+        // {
+        //     int maxColumn = 1;
+        //     PdfPTable pdfPTable = new PdfPTable(maxColumn);
+        //     string img = $"{Directory.GetCurrentDirectory()}{@"/wwwroot/Images/logo.png"}";
+        //     Image image = Image.GetInstance(img);
+
+        //     pdfCell = new PdfPCell(image);
+        //     pdfCell.Colspan = maxColumn;
+        //     pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+        //     pdfCell.Border = 0;
+        //     pdfCell.ExtraParagraphSpace = 0;
+        //     pdfPTable.AddCell(pdfCell);
+
+        //     pdfPTable.CompleteRow();
+
+        //     return pdfPTable;
+        // }
+
+        private PdfPTable setPageTitle()
+        {
+            PdfPTable pdfTable = new PdfPTable(2);
+
+            fontStyle = FontFactory.GetFont("Calibri", 18f, 1);
+            fontFecha = FontFactory.GetFont("Calibri", 10f, 1);
+            fontTitulo = FontFactory.GetFont("Calibri", 25f, 1);
+
+            pdfCell = new PdfPCell(new Phrase("SweetDereams, srl", fontTitulo));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Colspan = 2;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            pdfCell = new PdfPCell(new Phrase("Reporte de Clientes", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Colspan = 2;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            pdfCell = new PdfPCell(new Phrase(DateTime.Now.ToString("dd/MM/yyyy H:mm tt"), fontFecha));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Colspan = 2;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            //Una fila en blanco
+            pdfCell = new PdfPCell(new Phrase(" ", fontStyle));
+            pdfCell.Colspan = 2;
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.Border = 0;
+            pdfCell.ExtraParagraphSpace = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+
+            return pdfTable;
+        }
+
+        private void ReportBody()
+        {
+            fontStyle = FontFactory.GetFont("Calibri", 9f, 1);
+            var _fontStyle = FontFactory.GetFont("Calibri", 9f, 0);
+
+            #region Table Header
+            pdfCell = new PdfPCell(new Phrase("HabitacionId", fontStyle)); //HabitacionId
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Numero de habitacion", fontStyle)); //Numero de habitacion
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Tipo", fontStyle)); //Tipo
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Caracteristicas", fontStyle)); //Caractereristicas
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Precio", fontStyle)); //Precio
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Estado", fontStyle)); //Estado
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("Foto", fontStyle)); //Foto
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.LightGray;
+            pdfTable.AddCell(pdfCell);
+
+
+
+
+            pdfTable.CompleteRow();
+            #endregion
+
+            #region Table Body
+            int num = 0;
+
+            foreach (var item in ListaHabitaciones)
+            {
+                num++;
+                pdfCell = new PdfPCell(new Phrase(item.HabitacionId.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.NumeroHabitacion.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Tipo.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Caracteriscas.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Precio.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Estado.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+                pdfCell = new PdfPCell(new Phrase(item.Foto.ToString(), _fontStyle));
+                pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                pdfCell.BackgroundColor = BaseColor.White;
+                pdfTable.AddCell(pdfCell);
+
+
+
+                pdfTable.CompleteRow();
+            }
+
+            pdfCell = new PdfPCell(new Phrase("Total de Habitaciones", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase(num++.ToString(), fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfCell = new PdfPCell(new Phrase("            ", fontStyle));
+            pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            pdfCell.BackgroundColor = BaseColor.White;
+            pdfCell.Border = 0;
+            pdfTable.AddCell(pdfCell);
+
+            pdfTable.CompleteRow();
+            #endregion
+        }
+
+    }
+}
